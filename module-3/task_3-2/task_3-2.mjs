d4df0a96d5f0420f5bb8f6ba5c91644784ec1061
sondrebk2003 <sondrebergsvikknudsen01@gmail.com>
2025-10-13 13:28:10 +0200

    Solved part 1-4

--- module-3/task_3-2/task_3-2.mjs
+++ module-3/task_3-2/task_3-2.mjs
@@ -3,22 +3,62 @@ import { printOut, newLine } from "../../common/script/utils.mjs";
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+for (let i = 1; i <= 10; i++) {
+  printOut(i);
+}
+for (let i = 10; i >= 1; i--) {
+  printOut(i);
+}
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+let numberGuesser = Math.floor(Math.random() * 60) + 1;
+while (numberGuesser !== 42) {
+  numberGuesser = Math.floor(Math.random() * 60) + 1;
+  printOut(console.log(`Guessed number ${numberGuesser}`));
+  if (numberGuesser === 42) {
+    printOut("Guessed the correct number 42!");
+  }
+}
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+let guess = Math.floor(Math.random() * 1000000) + 1;
+let attempts = 0;
+const time = Date.now();
+while (guess !== 424242) {
+  guess = Math.floor(Math.random() * 1000000) + 1;
+  attempts++;
+}
+printOut(`Guessed the correct number 424242 in ${attempts} attempts! it took ${(Date.now() - time)} milliseconds!`);
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+function isPrimeNumber(num) {
+  for (let i = 2; i * i <= num; i++ ) {
+    if (num === 1) {return false};
+    if (num % i === 0) {return false};
+  }
+  return true;
+}
+for (let i = 2; i <= 200; i++) {
+  if (isPrimeNumber(i) === true) {
+    printOut(i);
+  }
+}
+
+let num = 2;
+let primes = [];
+while (num <= 200) {
+  if (isPrimeNumber(num) === true) {
+    primes.push(num); //it was supposed to put all in a nice line :/
+  }
+  num++;
+}
+printOut(primes);
 printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
