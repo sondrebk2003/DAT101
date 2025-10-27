f5f4c0f9f51572f100213b4190d190838e3f031c
sondrebk2003 <sondrebergsvikknudsen01@gmail.com>
2025-10-27 08:26:00 +0100

    idk lol

--- module-3/task_3-2/task_3-2.mjs
+++ module-3/task_3-2/task_3-2.mjs
@@ -3,12 +3,16 @@ import { printOut, newLine } from "../../common/script/utils.mjs";
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
+let aNumber = []
 for (let i = 1; i <= 10; i++) {
-  printOut(i);
+  aNumber.push(i);
 }
+printOut(aNumber.join(", "));
+aNumber = [];
 for (let i = 10; i >= 1; i--) {
-  printOut(i);
+  aNumber.push(i);
 }
+printOut(aNumber.join(", "));
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
@@ -65,15 +69,48 @@ printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-
-printOut("Replace this with you answer!");
+let num1 = 1;
+let num2 = 1;
+let row = [];
+for (let i = num2; i <= 7; i++) {
+  while (num1 <= 9) {
+    row.push("K" + num1 + "R" + num2)
+    num1++;
+  } 
+  num1 = 1;
+  printOut(row.join(", "));
+  row = [];
+  num2++;
+}
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+let maxScore = 236;
+let score = Math.floor(Math.random() * 236) + 1;
+let gradeA = maxScore * 0.89;
+let gradeB = maxScore * 0.77;
+let gradeC = maxScore * 0.65;
+let gradeD = maxScore * 0.53;
+let gradeE = maxScore * 0.41;
+let gradeF = maxScore * 0.40;
 
+for let i = 0; i <= 5; i++) {
+  if (score >= gradeA) {
+    printOut(`Score: ${score} Grade: A`);
+  } else if (score >= gradeB) {
+    printOut(`Score: ${score} Grade: B`);
+  } else if (score >= gradeC) {
+    printOut(`Score: ${score} Grade: C`);
+  } else if (score >= gradeD) {
+    printOut(`Score: ${score} Grade: D`);
+  } else if (score >= gradeE) {
+    printOut(`Score: ${score} Grade: E`);
+  } else {
+    printOut(`Score: ${score} Grade: F`);
+  }
+}
+printOut(newLine);
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
 printOut("Replace this with you answer!");
