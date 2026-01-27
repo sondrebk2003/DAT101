0aa81fb7268ff2e07f3928d6cc4e59a4d23923c3
sondrebk2003 <sondrebergsvikknudsen01@gmail.com>
2026-01-27 15:30:25 +0100

    You can now click the buttons yippie

--- module-5/Task_5-2_SimonSays/colorButton.js
+++ module-5/Task_5-2_SimonSays/colorButton.js
@@ -1,9 +1,42 @@
 "use strict";
 import { TSpriteButton } from "libSprite"
-import { SpriteInfoList } from "./SimonSays.mjs";
+import { TPoint } from "lib2d"
 
 export class TColorButton extends TSpriteButton {
-  constructor (aSpcvs, aSPI) {
+  #dst;
+  #gameBoardCenter
+  
+  constructor (aSpcvs, aSPI, aGameBoardCenter) {
     super (aSpcvs, aSPI, aSPI.dst.x, aSPI.dst.y)
+    this.#dst = aSPI.dst;
+    this.#gameBoardCenter = aGameBoardCenter
+  }
+
+  isMouseOver(aMousePos) {
+    const isOver = super.isMouseOver(aMousePos);
+    if (isOver) {
+      const dx = this.#gameBoardCenter.x - aMousePos.x;
+      const dy = this.#gameBoardCenter.y - aMousePos.y;
+      let hyp = Math.pow(dx, 2) + Math.pow(dy, 2);
+      hyp = Math.sqrt(hyp);
+      let inside = hyp > this.#dst.r1 && this.#dst.r2;
+      if (inside) {
+        console.log(true)
+        return true;
+      } else {
+        console.log(false)
+        return false;
+      }
+    }
+  }
+
+  onMouseDown() {
+    // No need to call super :3
+    this.index = 1;
+    
+  }
+
+  onMouseUp() {
+    this.index = 0;
   }
 }
--- module-5/Task_5-2_SimonSays/gameBoard.js
+++ module-5/Task_5-2_SimonSays/gameBoard.js
@@ -1,20 +1,24 @@
 "use strict";
 import { TSprite } from "libSprite";
 import { TColorButton } from "./colorButton.js";
-import { SpriteInfoList } from "./SimonSays.mjs"
+import { TPoint } from "lib2d";
 
 export class TGameBoard extends TSprite {
   #colorButtons
 
   constructor(aSpcvs, aSPI) {
     super(aSpcvs, aSPI.Background, 0, 0);
+    const center = new TPoint (
+      aSPI.Background.width / 2,
+      aSPI.Background.height / 2)
 
     this.#colorButtons = [
-      new TColorButton(aSpcvs, aSPI.ButtonRed),
-      new TColorButton(aSpcvs, aSPI.ButtonBlue),
-      new TColorButton(aSpcvs, aSPI.ButtonGreen),
-      new TColorButton(aSpcvs, aSPI.ButtonYellow)
+      new TColorButton(aSpcvs, aSPI.ButtonRed, center),
+      new TColorButton(aSpcvs, aSPI.ButtonBlue, center),
+      new TColorButton(aSpcvs, aSPI.ButtonGreen, center),
+      new TColorButton(aSpcvs, aSPI.ButtonYellow, center)
     ];
+    //this.#colorButtons[0].debug = true;
   }
 
   draw() {
